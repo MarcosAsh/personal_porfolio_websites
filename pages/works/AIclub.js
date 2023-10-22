53cd8e7f7d7a2b6dbb2f30d50a9c5eaf90b3a635
Marcos Ashton <59975445+MarcosAsh@users.noreply.github.com>
2023-10-22 16:54:29 +0200

    Add files via upload

--- pages/works/AIclub.js
+++ pages/works/AIclub.js
@@ -3,10 +3,7 @@ import {
   Badge,
   Link,
   List,
-  ListItem,
-  UnorderedList,
-  Heading,
-  Center
+  ListItem
 } from '@chakra-ui/react'
 import Layout from '../../components/layouts/article'
 import { ExternalLinkIcon } from '@chakra-ui/icons'
--- pages/works/andriod.js
+++ pages/works/andriod.js
@@ -1,14 +1,9 @@
 import {
   Container,
-  Badge,
-  Link,
-  List,
-  ListItem,
-  SimpleGrid
+  Badge
 } from '@chakra-ui/react'
 import Layout from '../../components/layouts/article'
-import { ExternalLinkIcon } from '@chakra-ui/icons'
-import { Title, WorkImage, Meta } from '../../components/work'
+import { Title } from '../../components/work'
 import P from '../../components/paragraph'
 
 const Work = () => (
